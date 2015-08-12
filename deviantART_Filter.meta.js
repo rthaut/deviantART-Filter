e0acba05b4fcc0ff48ba2d499f26f58951115053
Ryan Thaut <rthaut@gmail.com>
2015-08-11 19:51:38 -0600

    Separated script meta for update URL

---
+++ deviantART_Filter.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        deviantART Filter
+// @author      Ryan Thaut
+// @description Allows automatic filtering of deviations from certain users and/or in certain categories.
+// @namespace   http://repo.ryanthaut.com/userscripts/deviantart_filter
+// @updateURL   http://repo.ryanthaut.com/userscripts/deviantart_filter/deviantART_Filter.user.js
+// @downloadURL http://repo.ryanthaut.com/userscripts/deviantart_filter/deviantART_Filter.user.js
+// @include     http://*deviantart.com/*
+// @version     1.5.1
+// @grant       GM_addStyle
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @require     http://code.jquery.com/jquery-latest.min.js
+// ==/UserScript==
