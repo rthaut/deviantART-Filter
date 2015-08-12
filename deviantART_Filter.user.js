1c99b19e8d770fc234d4087578b03705b65c5af9
Ryan Thaut <rthaut@gmail.com>
2015-08-11 19:57:28 -0600

    Removed Beta version and update URLs for 2.0 release

--- deviantART_Filter.user.js
+++ deviantART_Filter.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name        deviantART Filter Beta
+// @name        deviantART Filter
 // @author      Ryan Thaut
 // @description Allows automatic filtering of deviations from certain users and/or in certain categories.
 // @namespace   http://repo.ryanthaut.com/userscripts/deviantart_filter
-// @updateURL   http://repo.ryanthaut.com/userscripts/deviantart_filter/deviantART_Filter_Beta.user.js
-// @downloadURL http://repo.ryanthaut.com/userscripts/deviantart_filter/deviantART_Filter_Beta.user.js
+// @updateURL   http://repo.ryanthaut.com/userscripts/deviantart_filter/deviantART_Filter.meta.js
+// @downloadURL http://repo.ryanthaut.com/userscripts/deviantart_filter/deviantART_Filter.user.js
 // @include     http://*deviantart.com/*
-// @version     2.0b6
+// @version     2.0
 // @grant       GM_addStyle
 // @grant       GM_getValue
 // @grant       GM_setValue
