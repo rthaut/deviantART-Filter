f59d95fda1e6f41a9dab1fcc1fbc38be5b9dc4e0
rthaut <rthaut@gmail.com>
2017-09-14 08:04:34 -0600

    simplified file names and moved vendor files to their own directory

--- gulpfile.js
+++ gulpfile.js
@@ -30,8 +30,8 @@ var zip = require('gulp-zip');
 // set to TRUE to enable console messages in JS output files
 var DEBUG = true;
 
-// included libraries needed for core functionality
-var includes = [
+// vendor libraries needed for core functionality
+var vendor = [
     './node_modules/angular/angular-csp.css',
     './node_modules/angular/angular.min.js',
     './node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js',
@@ -40,6 +40,10 @@ var includes = [
     './node_modules/bootstrap/dist/js/bootstrap.min.js',
     './node_modules/jquery/dist/jquery.min.js',
     './node_modules/webextension-polyfill/dist/browser-polyfill.min.js',
+]
+
+// additional includes (custom libraries, classes, etc.)
+var includes = [
     './lib/jquery.daModal.js',
 ]
 
@@ -70,7 +74,8 @@ var options = {
 var _folders = {
     locales: './_locales',
     components: './lib/components',
-    pages: './lib/pages'
+    pages: './lib/pages',
+    scripts: './lib/scripts'
 };
 
 
@@ -97,8 +102,10 @@ gulp.task('clean', function () {
 gulp.task('build:components', folders(_folders.components, function (folder) {
     return gulp.src(path.join(_folders.components, folder, '**/*.js'))
         .pipe(embedTemplates())
-        .pipe(gulpIf(['*.js', '!*.min.js'], uglify(options.uglify)))
-        .pipe(rename(folder + '.min.js'))
+        .pipe(concat(folder + '.js'))
+        .pipe(uglify(options.uglify))
+        //.pipe(rename(folder + '.min.js'))
+        .pipe(header(fs.readFileSync('./banner.txt', 'utf8'), { package: package }))
         .pipe(gulp.dest('./dist/components'));
 }));
 gulp.task('build:icons', function () {
@@ -107,13 +114,13 @@ gulp.task('build:icons', function () {
 });
 gulp.task('build:includes', function () {
     return gulp.src(includes)
-        .pipe(gulpIf(['*.js', '!*.min.js'], uglify(options.uglify)))
         .pipe(gulp.dest('./dist/includes'));
 });
 gulp.task('build:less', function () {
     return gulp.src('./lib/less/*.less')
         .pipe(less(options.less))
-        .pipe(gulpIf(['*.css', '!*.min.css'], rename({ suffix: '.min' })))
+        //.pipe(rename({ suffix: '.min' }))
+        .pipe(header(fs.readFileSync('./banner.txt', 'utf8'), { package: package }))
         .pipe(gulp.dest('./dist/css'));
 });
 gulp.task('build:locales', folders(_folders.locales, function (folder) {
@@ -131,27 +138,30 @@ gulp.task('build:manifest', function () {
 gulp.task('build:pages', folders(_folders.pages, function (folder) {
     return merge(
         gulp.src(path.join(_folders.pages, folder, '**/*.html'))
-            .pipe(concat(package.name + '.' + folder + '.html')),
+            .pipe(concat(folder + '.html')),
         gulp.src(path.join(_folders.pages, folder, '**/*.css'))
-            .pipe(concat(package.name + '.' + folder + '.css'))
+            .pipe(concat(folder + '.css'))
+            //.pipe(rename({ suffix: '.min' }))
             .pipe(header(fs.readFileSync('./banner.txt', 'utf8'), { package: package })),
         gulp.src(path.join(_folders.pages, folder, '**/*.js'))
-            .pipe(concat(package.name + '.' + folder + '.js'))
+            .pipe(concat(folder + '.js'))
             .pipe(uglify(options.uglify))
+            //.pipe(rename(folder + '.min.js'))
             .pipe(header(fs.readFileSync('./banner.txt', 'utf8'), { package: package }))
     )
         .pipe(gulp.dest(path.join('./dist/pages', folder)));
 }));
-gulp.task('build:scripts', function () {
-    return merge(
-        gulp.src('./lib/scripts/background/**/*.js')
-            .pipe(concat(package.name + '.background.js')),
-        gulp.src('./lib/scripts/content/**/*.js')
-            .pipe(concat(package.name + '.content.js'))
-    )
-        .pipe(gulpIf(['*.js', '!*.min.js'], uglify(options.uglify)))
+gulp.task('build:scripts', folders(_folders.scripts, function (folder) {
+    return gulp.src(path.join(_folders.scripts, folder, '**/*.js'))
+        .pipe(concat(folder + '.js'))
+        .pipe(uglify(options.uglify))
+        //.pipe(rename(folder + '.min.js'))
         .pipe(header(fs.readFileSync('./banner.txt', 'utf8'), { package: package }))
         .pipe(gulp.dest('./dist/scripts'));
+}));
+gulp.task('build:vendor', function () {
+    return gulp.src(vendor)
+        .pipe(gulp.dest('./dist/vendor'));
 });
 
 
@@ -175,7 +185,7 @@ gulp.task('build', ['clean'], function (callback) {
     sequence(
         ['build:icons', 'build:less', 'build:locales', 'build:manifest'],
         ['lint'],
-        ['build:components', 'build:includes', 'build:pages', 'build:scripts'],
+        ['build:components', 'build:includes', 'build:pages', 'build:scripts', 'build:vendor'],
         //['zip', 'crx'],
         callback
     );
@@ -186,6 +196,7 @@ gulp.task('watch', ['build'], function () {
     debug = true;
     gulp.watch(path.join(_folders.components, '/**/*'), ['build:components']);
     gulp.watch('./icons/**/*.{png,svg}', ['build:icons']);
+    gulp.watch(includes, ['build:includes']);
     gulp.watch('./lib/less/**/*.less', ['build:less']);
     gulp.watch(path.join(_folders.locales, '/**/*'), ['build:locales']);
     gulp.watch('./manifest.json', ['build:manifest']);
