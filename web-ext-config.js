f08f9f26f221e65c87198481833e34b78f531106
rthaut <rthaut@gmail.com>
2018-05-02 10:10:52 -0600

    implemented config file for web-ext

---
+++ web-ext-config.js
@@ -0,0 +1,15 @@
+/* global module */
+module.exports = {
+    'artifactsDir': './dist',
+    'build': {
+        'overwriteDest': true,
+    },
+    'run': {
+        'browserConsole': true,
+        'firefox': 'firefox',
+        'startUrl': [
+            'www.deviantart.com'
+        ]
+    },
+    'sourceDir': './dist/firefox'
+};
