95cc8bd0b1ce6958614e1a31167e494aa75b7b28
Ryan Thaut <rthaut@gmail.com>
2020-04-08 17:31:48 -0600

    (WIP) add button to clear all filters

--- app/scripts/manage/components/FilterTable.jsx
+++ app/scripts/manage/components/FilterTable.jsx
@@ -3,6 +3,18 @@ import React, { forwardRef, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import MaterialTable from 'material-table';
 
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+} from '@material-ui/core';
+
 import {
     AddBox,
     ArrowDownward,
@@ -11,6 +23,7 @@ import {
     ChevronRight,
     Clear,
     DeleteOutline,
+    DeleteSweep,
     Edit,
     FilterList,
     FirstPage,
@@ -21,11 +34,14 @@ import {
     ViewColumn,
 } from '@material-ui/icons';
 
+import { ADD_FILTER, REMOVE_FILTER, SAVE_FILTER, UPDATE_FILTER } from '../../constants/messages';
+
 const tableIcons = {
     'Add': forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
     'Check': forwardRef((props, ref) => <Check {...props} ref={ref} />),
     'Clear': forwardRef((props, ref) => <Clear {...props} ref={ref} />),
     'Delete': forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    'DeleteSweep': forwardRef((props, ref) => <DeleteSweep {...props} ref={ref} />),
     'DetailPanel': forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
     'Edit': forwardRef((props, ref) => <Edit {...props} ref={ref} />),
     'Export': forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
@@ -41,7 +57,13 @@ const tableIcons = {
     'ViewColumn': forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
 };
 
-const FilterTable = ({filterKey, columns, ...rest}) => {
+const errorTheme = createMuiTheme({
+    'palette': {
+        'primary': red,
+    },
+});
+
+const FilterTable = ({ filterKey, columns, ...rest }) => {
 
     const onStorageChanged = (changes, areaName) => {
         console.time('onStorageChanged()');
@@ -85,43 +107,88 @@ const FilterTable = ({filterKey, columns, ...rest}) => {
         console.timeEnd('sendFilterMessage()');
     };
 
+    const [confirmation, setConfirmation] = useState('');
+
+    const confirmReset = () => {
+        setConfirmation(`Are you sure you want to delete all ${data.length} of your ${filterKey} filters?`);
+    };
+
+    const resetFilter = async () => {
+        await sendFilterMessage(SAVE_FILTER, []);
+        setData([]);
+        setConfirmation('');
+    };
+
+    const closeConfirmation = () => {
+        setConfirmation('');
+    };
+
     const stripTableData = ({ tableData, ...data }) => data;
 
     return (
-        <MaterialTable
-            icons={tableIcons}
-            columns={columns}
-            data={data}
-            options={{
-                'draggable': false,
-                'pageSize': 10,
-                'pageSizeOptions': [10, 25, 50, 100]
-            }}
-            editable={{
-                'onRowAdd': (newData) => new Promise((resolve, reject) => {
-                    const newFilterData = stripTableData(newData);
+        <>
+            <MaterialTable
+                icons={tableIcons}
+                columns={columns}
+                data={data}
+                options={{
+                    'draggable': false,
+                    'pageSize': 10,
+                    'pageSizeOptions': [10, 25, 50, 100],
+                }}
+                editable={{
+                    'onRowAdd': (newData) => new Promise((resolve, reject) => {
+                        const newFilterData = stripTableData(newData);
 
-                    sendFilterMessage('ADD_FILTER', newFilterData).then(resolve, reject);
-                }),
-                'onRowDelete': (oldData) => new Promise((resolve, reject) => {
-                    const oldFilterData = stripTableData(oldData);
+                        sendFilterMessage(ADD_FILTER, newFilterData).then(resolve, reject);
+                    }),
+                    'onRowDelete': (oldData) => new Promise((resolve, reject) => {
+                        const oldFilterData = stripTableData(oldData);
 
-                    sendFilterMessage('REMOVE_FILTER', oldFilterData).then(resolve, reject);
-                }),
-                'onRowUpdate': (newData, oldData) => new Promise((resolve, reject) => {
-                    const newFilterData = stripTableData(newData);
-                    const oldFilterData = stripTableData(oldData);
+                        sendFilterMessage(REMOVE_FILTER, oldFilterData).then(resolve, reject);
+                    }),
+                    'onRowUpdate': (newData, oldData) => new Promise((resolve, reject) => {
+                        const newFilterData = stripTableData(newData);
+                        const oldFilterData = stripTableData(oldData);
 
-                    const value = {
-                        'old': oldFilterData,
-                        'new': newFilterData
-                    };
+                        const value = {
+                            'old': oldFilterData,
+                            'new': newFilterData
+                        };
 
-                    sendFilterMessage('UPDATE_FILTER', value).then(resolve, reject);
-                })
-            }}
-            {...rest}
-        />
+                        sendFilterMessage(UPDATE_FILTER, value).then(resolve, reject);
+                    })
+                }}
+                actions={[
+                    {
+                        'icon': tableIcons.DeleteSweep,
+                        'tooltip': 'Delete All',
+                        'onClick': () => confirmReset(data),
+                        'isFreeAction': true,
+                        'disabled': data.length < 2
+                    }
+                ]}
+                {...rest}
+            />
+            <Dialog open={(confirmation?.length > 1)} onClose={closeConfirmation}>
+                <DialogTitle>Confirm</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        {confirmation}
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={closeConfirmation} color="primary" variant="contained" autoFocus>
+                        No
+                    </Button>
+                    <ThemeProvider theme={errorTheme}>
+                        <Button onClick={resetFilter} color="primary" variant="outlined">
+                            Yes
+                        </Button>
+                    </ThemeProvider>
+                </DialogActions>
+            </Dialog>
+        </>
     );
 
 };
