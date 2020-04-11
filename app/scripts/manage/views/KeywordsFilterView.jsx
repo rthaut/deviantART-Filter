6b2b7bbb1a194338e08b26a77293696ed7ea4587
Ryan Thaut <rthaut@gmail.com>
2020-04-10 22:33:03 -0600

    tweak input fields for Keywords and Users on filter tables

--- app/scripts/manage/views/KeywordsFilterView.jsx
+++ app/scripts/manage/views/KeywordsFilterView.jsx
@@ -1,27 +1,46 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import { MTableEditField } from 'material-table';
+
 import FilterTable from '../components/FilterTable';
 
-const KeywordsFilterView = () => {
-    const columns = [
-        {
-            'title': 'Keyword',
-            'field': 'keyword'
-        },
-        {
-            'title': 'Is Wildcard',
-            'field': 'wildcard',
-            'type': 'boolean'
-        }
-    ];
+class KeywordsFilterView extends React.Component {
 
-    return (
-        <Grid container spacing={3}>
-            <Grid item xs={12}>
-                <FilterTable columns={columns} filterKey='keywords' title='Filtered Keywords' />
+    constructor(props) {
+        super(props);
+        this.state = {
+            'columns': [
+                {
+                    'title': 'Keyword',
+                    'field': 'keyword',
+                    'editComponent': props => (
+                        // TODO: use a library, like react-text-mask, to enforce pattern and/or show validity?
+                        <MTableEditField {...props} inputProps={{ 'pattern': '[a-zA-Z0-9]+\\S*', 'required': true }} />
+                    ),
+                },
+                {
+                    'title': 'Is Wildcard',
+                    'field': 'wildcard',
+                    'type': 'boolean',
+                    'editComponent': props => (
+                        <MTableEditField {...props} color="primary" />
+                    ),
+                }
+            ]
+        };
+    }
+
+    render() {
+        return (
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <FilterTable columns={this.state.columns} filterKey='keywords' title='Filtered Keywords' />
+                </Grid>
             </Grid>
-        </Grid>
-    );
-};
+        );
+    }
+
+}
 
 export default KeywordsFilterView;
--- app/scripts/manage/views/UsersFilterView.jsx
+++ app/scripts/manage/views/UsersFilterView.jsx
@@ -1,22 +1,38 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { Grid } from '@material-ui/core';
+import { MTableEditField } from 'material-table';
+
 import FilterTable from '../components/FilterTable';
 
-const UsersFilterView = () => {
-    const columns = [
-        {
-            'title': 'Username',
-            'field': 'username'
-        }
-    ];
+class UsersFilterView extends React.Component {
 
-    return (
-        <Grid container spacing={3}>
-            <Grid item xs={12}>
-                <FilterTable columns={columns} filterKey='users' title='Filtered Users' />
+    constructor(props) {
+        super(props);
+        this.state = {
+            'columns': [
+                {
+                    'title': 'Username',
+                    'field': 'username',
+                    'editComponent': props => (
+                        // TODO: use a library, like react-text-mask, to enforce pattern and/or show validity?
+                        <MTableEditField {...props} inputProps={{ 'pattern': '[\\w\\-]+', 'required': true }} />
+                    ),
+                }
+            ]
+        };
+    }
+
+    render() {
+        return (
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <FilterTable columns={this.state.columns} filterKey='users' title='Filtered Users' />
+                </Grid>
             </Grid>
-        </Grid>
-    );
-};
+        );
+    }
+
+}
 
 export default UsersFilterView;
