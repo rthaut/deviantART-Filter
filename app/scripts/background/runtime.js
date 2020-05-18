1e3d80255aac56a0c6baf421ddd902ad9c4e22ca
Ryan Thaut <rthaut@gmail.com>
2020-05-17 21:13:42 -0600

    open installation page on new install & open corresponding release page on update

--- app/scripts/background/runtime.js
+++ app/scripts/background/runtime.js
@@ -1,32 +1,73 @@
+import semverClean from 'semver/functions/clean';
 import semverLT from 'semver/functions/lt';
-import semverValid from 'semver/functions/valid';
 
 import { GetCategories } from './categories';
 import { ImportFilters } from './filters';
 
 import { TAG_FILTERS_MIGRATED } from '../constants/notifications';
 
-export const OnInstalled = async (details) => {
-    const { previousVersion } = details;
+export const OnInstalled = ({ previousVersion, reason, temporary }) => {
 
-    if (semverValid(previousVersion) && semverLT(previousVersion, '6.0.0')) {
-        const data = await browser.storage.local.get('tags');
-        if (data?.tags) {
-            console.warn('Converting tag filters to keyword filters');
-            await ImportFilters(data);
+    // fetch and store the latest category paths
+    GetCategories();
 
-            // TODO: is it appropriate to delete tag filters from previous versions?
-            // await browser.storage.local.remove('tags');
+    if (temporary) {
+        // use this to simulate installs/updates for testing purposes
+        // previousVersion = '6.0.0';
+        // reason = 'install';
+    }
 
-            browser.notifications.create(TAG_FILTERS_MIGRATED, {
-                'type': 'basic',
-                'iconUrl': browser.extension.getURL('images/icon-64.png'),
-                'title': browser.i18n.getMessage('ExtensionName'),
-                'message': browser.i18n.getMessage('TagFiltersMigratedNotificationMessage'),
-            });
+    switch (reason) {
+        case 'install':
+            ShowInstalledPage();
+            break;
+
+        case 'update':
+            OnUpdated(previousVersion);
+            break;
+    }
+};
+
+const OnUpdated = async (previousVersion) => {
+    let { 'version': currentVersion } = await browser.management.getSelf();
+
+    currentVersion = semverClean(currentVersion);
+    previousVersion = semverClean(previousVersion);
+
+    if (previousVersion !== null) {
+        if (semverLT(previousVersion, '6.0.0')) {
+            await MigrateTagFiltersToKeywordFilters();
         }
     }
 
-    // fetch and store the latest category paths
-    await GetCategories();
+    await ShowUpdatedPage(currentVersion, previousVersion);
+};
+
+const MigrateTagFiltersToKeywordFilters = async () => {
+    const data = await browser.storage.local.get('tags');
+    if (data?.tags) {
+        console.warn('Migrating tag filters to keyword filters');
+        await ImportFilters(data);
+
+        // TODO: is it appropriate to delete tag filters from previous versions?
+        // await browser.storage.local.remove('tags');
+
+        browser.notifications.create(TAG_FILTERS_MIGRATED, {
+            'type': 'basic',
+            'iconUrl': browser.extension.getURL('images/icon-64.png'),
+            'title': browser.i18n.getMessage('ExtensionName'),
+            'message': browser.i18n.getMessage('TagFiltersMigratedNotificationMessage'),
+        });
+    }
+};
+
+const ShowInstalledPage = async () => {
+    const url = 'http://localhost:4000/installed';
+    await browser.tabs.create({ url });
+};
+
+
+const ShowUpdatedPage = async (currentVersion, previousVersion) => {
+    const url = `http://localhost:4000/releases/v${currentVersion}/?from=v${previousVersion}`;
+    await browser.tabs.create({ url });
 };
